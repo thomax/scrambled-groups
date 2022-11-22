b3e0cf1e4ae246be54bf44213d75afdcba156465
Thomas Drevon <thdra003@osloskolen.no>
2022-11-22 16:23:43 +0100

    Begun refactoring the store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,13 +1,29 @@
-import {writable} from 'svelte/store'
+import { writable } from 'svelte/store'
 import sampleGroups from './sampleGroups.txt?raw'
 
 // Get the value out of local storage on load
 // Use a sane default
 const stored = localStorage.groups || sampleGroups
 
+export let allGroups = [[]]
+export let selectedGroup = allGroups[0]
+export let membersByUnit = [[]]
+
+
+// Used by StoredGroupsEditor
 export const storedGroups = writable(stored)
 
-// Any time the store changes, update local storage
+// Whenever conent of LocalStorage changes
 storedGroups.subscribe((value) => {
   localStorage.groups = value
+
+  // Update allGroups
+  allGroups = value.split('\n').map((rawGroup) => {
+    const [name, rawMembers] = rawGroup.split(':')
+    const members = rawMembers 
+      ? rawMembers.split(',').map((member) => member.trim()) 
+      : []
+    return { name: name.trim(), members }
+  })
 })
+
