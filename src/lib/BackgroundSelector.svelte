18c7569e88a1020829b0d2adbea8e5c639097da7
Thomas Drevon <thomas@drevon.no>
2023-10-02 09:37:24 +0200

    Fix the bg selector thingy

--- src/lib/BackgroundSelector.svelte
+++ src/lib/BackgroundSelector.svelte
@@ -2,15 +2,17 @@
   import {selectedBackground} from './stores.js'
   import {updateBackground, backgroundImages} from './utils'
 
-  const handleChangeBackground = () => {
+  const handleChangeBackground = (index) => {
+    $selectedBackground = index
     updateBackground($selectedBackground)
   }
 </script>
 
-<input
-  type="range"
-  min="0"
-  max={backgroundImages.length - 1}
-  bind:value={$selectedBackground}
-  on:change={handleChangeBackground}
-/>
+<div class="backgroundImageList">
+  {#each backgroundImages as imgUrl, index}
+    <input type="radio" name="bgImage" checked={$selectedBackground === index} />
+    <label for={`backgroundImage-${index}`} on:click={() => handleChangeBackground(index)}>
+      <img width="20" src={imgUrl} alt={`backgroundImage-${index}`} />
+    </label>
+  {/each}
+</div>
